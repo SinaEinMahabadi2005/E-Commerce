dbee683a5776f46fd21ad3869cc070ea3b6296ce
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2025-11-30 20:44:50 +0330

    Update

--- client/src/Layout/index.jsx
+++ client/src/Layout/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../Components/Footer'
 
 export default function Layout() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
   )
 }
